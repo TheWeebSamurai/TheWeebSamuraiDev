78dcf288093ad5685ef794d12b72f5948af22080
TheWeebSamurai <68552289+TheWeebSamurai@users.noreply.github.com>
2022-01-24 20:49:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function redirect(){
- document.location.href = 'https://theweebsamurai.github.io/twsdev/redirect.html' 
+ document.location.href = 'https://theweebsamurai.github.io/twsdev/redirect' 
 }
 
 //IS it working or not ? 
